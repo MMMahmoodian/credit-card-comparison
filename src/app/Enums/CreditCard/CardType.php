b42199e4823f88c72d6c8ebab42ba4e1a772505e
Mahdi Mahmoodian <mahmoodian.m1999@gmail.com>
2025-02-27 02:15:29 +0330

    [chore] add toString method of CardType enum

--- src/app/Enums/CreditCard/CardType.php
+++ src/app/Enums/CreditCard/CardType.php
@@ -6,4 +6,12 @@ enum CardType: int
 {
     case CREDIT = 0;
     case DEBIT = 2;
+
+    public function toString(): string
+    {
+        return match ($this) {
+            self::CREDIT => 'Credit',
+            self::DEBIT => 'Debit',
+        };
+    }
 }
