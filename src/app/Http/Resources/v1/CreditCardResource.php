8f1f7acc434f23715034d605379c6011b2ed8d53
Mahdi Mahmoodian <mahmoodian.m1999@gmail.com>
2025-02-27 02:47:12 +0330

    [fix] fix issue with strip_tags not working

--- src/app/Http/Resources/v1/CreditCardResource.php
+++ src/app/Http/Resources/v1/CreditCardResource.php
@@ -22,8 +22,25 @@ class CreditCardResource extends JsonResource
             'fee_first_year' => $this->fee_first_year,
             'fee_second_year' => $this->fee_second_year,
             'card_type' => $this->card_type->toString(),
-            'special_offers' => strip_tags($this->special_offers),
-            'extra_info' => strip_tags($this->extra_info),
+            'special_offers' => strip_tags($this->decodeHtmlEntities($this->special_offers)),
+            'extra_info' => strip_tags($this->decodeHtmlEntities($this->extra_info)),
         ];
     }
+
+    /**
+     * The CSV output from FinanceAds uses semicolons as separators. This means that the HTML entities are not
+     * properly terminated. This is a workaround to fix this issue.
+     * This method is similar implementation to the PHP function html_entity_decode() just without the termination semicolon.
+     *
+     * @see https://stackoverflow.com/a/23195167/10798812
+     */
+    private function decodeHtmlEntities(string $input): string
+    {
+
+        $mapping = get_html_translation_table(HTML_ENTITIES, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+        array_walk($mapping, function(&$value) { $value = '/'.rtrim($value, ';').'(?!;)/'; });
+
+        return preg_replace(array_values($mapping), array_keys($mapping), $input);
+    }
+
 }
